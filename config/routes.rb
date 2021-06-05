cd7678d3267f10f5b2168a240b3545f35ca06639
takasuandcars <takasu.and.cars@gmail.com>
2021-06-05 03:01:03 +0000

    install fullcalendar and run npx browserslist@latest --update-db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+    get 'events/index', to: "events#index"
     get 'record_pdfs/index'
     root "static_pages#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
