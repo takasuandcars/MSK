2e1fed2b310a3cade9068117da06b69256f7ab08
takasuandcars <takasu.and.cars@gmail.com>
2021-06-18 05:29:24 +0000

    add info for the way to ship

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+    
+    def pickup_exist?(received_id)
+        received_data = Received.find_by(id: received_id)
+        pickup = received_data.pickup_request
+        
+        if pickup
+            return true
+        else
+            return false
+        end
+        
+    end
 end
