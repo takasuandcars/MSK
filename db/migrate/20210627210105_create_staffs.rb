fd926d12ff805c0f6a1daed7cf8c417fff5cd95a
takasuandcars <takasu.and.cars@gmail.com>
2021-06-27 21:01:32 +0000

    made staff model

---
+++ db/migrate/20210627210105_create_staffs.rb
@@ -0,0 +1,10 @@
+class CreateStaffs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :staffs do |t|
+      t.string :name
+      t.string :level
+
+      t.timestamps
+    end
+  end
+end
