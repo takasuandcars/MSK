39f0126bff8e1b851e2fbc4f19c7c2164e214cef
takasuandcars <takasu.and.cars@gmail.com>
2021-07-14 21:20:05 +0000

    show data on chart automatically

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,5 +1,11 @@
 class WorksController < ApplicationController
   def index
+    @work = WorkTime.first
+    @day = Date.today
+    
+    @monday = @day - (@day.wday - 1)
+    
+
   end
 
   def show
