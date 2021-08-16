69a59de132865ca72bd0c9470412c972500cea76
takasuandcars <takasu.and.cars@gmail.com>
2021-08-16 20:17:17 +0000

    not shown already invited user

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,2 +1,12 @@
 module EntriesHelper
+    
+    def entried?(user)
+        entries = user.entries.where(room_id: @room.id)
+        if entries.empty?
+            return false
+        else
+            return true
+        end
+    end
+        
 end
