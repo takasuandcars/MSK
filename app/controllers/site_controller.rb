fb8c8e854e98b1335406281d244d010e9e393f75
satoshi isogai <satoshi.isogai30@gmail.com>
2022-04-22 02:41:59 +0000

    changed the map for the first page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -95,157 +95,64 @@ class SiteController < ApplicationController
                 },
                 {
                     "id": "nopointer-logos",
-                    "title": "Logos",
+                    "title": "Area Name",
                     "toggle": "true",
                     "legend": "true",
                     "hide": "true"
                 }
             ],
             "levels": [
+               
                 {
-                    "id": "underground",
-                    "title": "Underground",
-                    "map": "/map/mall-underground.svg",
-                    "minimap": "maps/mall/mall-underground-mini.jpg",
-                    "locations": [
-                        {
-                            "id": "gap",
-                            "title": "GAP",
-                            "about": "Lorem ipsum",
-                            "description": "Lorem ipsum",
-                            "category": "fashion",
-                            "x": "0.3781",
-                            "y": "0.4296"
-                        },
-                        {
-                            "id": "petco",
-                            "title": "Petco",
-                            "about": "Lorem ipsum",
-                            "description": "Lorem ipsum",
-                            "category": "others",
-                            "style": "dark",
-                            "x": "0.5311",
-                            "y": "0.2686"
-                        }
-                    ]
-                },
-                {
-                    "id": "ground-floor",
-                    "title": "Ground Floor",
-                    "map": "/map/mall-ground.svg",
+                    "id": "second-floor",
+                    "title": "Second Floor",
+                    "map": "/map/logi-2.svg",
                     "minimap": "maps/mall/mall-ground-mini.jpg",
                     "show": "true",
                     "locations": [
                         {
-                            "id": "nordstrom",
-                            "title": "Nordstrom",
-                            "about": "Lorem ipsum dolor sit amet.",
-                            "description": "<p>Recognizing the need is the primary condition for design.</p><strong>10:00 - 22:00</strong> <small>(Monday - Sunday)</small>",
-                            "category": "fashion,dep",
-                            "style": "dark",
-                            "x": "0.7856",
-                            "y": "0.2022",
-                            "zoom": "2"
-                        },
-                        {
-                            "id": "macys",
-                            "title": "Macy's",
-                            "description": "<p>Recognizing the need is the primary condition for design.</p><strong>10:00 - 22:00</strong> <small>(Monday - Sunday)</small>",
-                            "category": "dep",
-                            "x": "0.2021",
-                            "y": "0.5847",
-                            "zoom": "5"
-                        },
-                        {
-                            "id": "jcpenney",
-                            "title": "JCPenney",
+                            "id": "area8",
+                            "title": "<h4>area8</h4>",
                             "about": "Lorem ipsum",
-                            "description": "<p>Recognizing the need is the primary condition for design.</p><strong>10:00 - 22:00</strong> <small>(Monday - Sunday)</small>",
-                            "category": "dep",
-                            "link": "https://1.envato.market/R5Nv",
-                            "x": "0.6651",
-                            "y": "0.6734",
-                            "zoom": "2"
-                        },
-                        {
-                            "id": "walgreens",
-                            "title": "Walgreens",
-                            "about": "Lorem ipsum",
-                            "description": "At the corner of Happy & Healthy",
-                            "image": "https://source.unsplash.com/v113g-WnhTI/400x150",
-                            "category": "health",
-                            "x": "0.4611",
-                            "y": "0.5426"
-                        },
-                        {
-                            "id": "sephora",
-                            "title": "Sephora",
-                            "about": "Lorem ipsum",
-                            "description": "<p>Makeup, fragrance, skincare</p><strong>10:00 - 22:00</strong> <small>(Monday - Sunday)</small>",
-                            "image": "https://source.unsplash.com/v113g-WnhTI/400x150",
-                            "category": "health",
-                            "link": "https://1.envato.market/R5Nv",
-                            "x": "0.7504",
-                            "y": "0.5211"
-                        },
-                        {
-                            "id": "geox",
-                            "title": "Geox",
-                            "about": "Lorem ipsum",
-                            "description": "<p>Recognizing the need is the primary condition for design.</p><strong>10:00 - 22:00</strong> <small>(Monday - Sunday)</small>",
-                            "category": "fashion",
-                            "link": "https://1.envato.market/R5Nv",
-                            "x": "0.3947",
-                            "y": "0.5544"
-                        },
-                        {
-                            "id": "hnm",
-                            "title": "H&M",
-                            "about": "Lorem ipsum",
-                            "description": "<p>Recognizing the need is the primary condition for design.</p><strong>10:00 - 22:00</strong> <small>(Monday - Sunday)</small>",
-                            "category": "fashion",
-                            "x": "0.6445",
-                            "y": "0.4478"
-                        },
-                        {
-                            "id": "adidas",
-                            "title": "Adidas",
-                            "about": "Lorem ipsum",
-                            "description": "<p>Recognizing the need is the primary condition for design.</p><strong>10:00 - 22:00</strong> <small>(Monday - Sunday)</small>",
-                            "category": "fashion",
-                            "x": "0.3688",
-                            "y": "0.3909"
-                        },
-                        {
-                            "id": "massimodutti",
-                            "title": "Massimo Dutti",
-                            "about": "Lorem ipsum",
-                            "description": "<p>Some people think design means how it looks. But of course, if you dig deeper, it's really how it works.</p><strong>10:00 - 22:00</strong> <small>(Monday - Sunday)</small>",
-                            "category": "fashion",
-                            "x": "0.6243",
-                            "y": "0.2655"
-                        },
-                        {
-                            "id": "starbucks",
-                            "title": "Starbucks",
-                            "about": "Lorem ipsum",
-                            "description": "<p>Enjoy today and every day with our creamy non-dairy beverage options.</p><strong>7:30 - 22:00</strong> <small>(Monday - Sunday)</small>",
-                            "image": "https://source.unsplash.com/n-HtQS7IgU4/400x150",
-                            "link": "https://www.mapplic.com/",
+                            "description": "<p>まだ、データは取得できません。</p><p>改良中</p>",
                             "category": "food",
-                            "x": "0.5431",
-                            "y": "0.5240"
+                            "style": "dark",
+                            "x": "0.2390",
+                            "y": "0.6000"
                         },
                         {
-                            "id": "zara",
-                            "title": "Zara",
+                            "id": "area7",
+                            "title": "<h4>Are7</h4><p>現在の作業人数：#{n_of_staffs}</p><p>本日の作業量：#{n_of_jobs}",
                             "about": "Lorem ipsum",
-                            "description": "<p>Recognizing the need is the primary condition for design.</p><strong>10:00 - 22:00</strong> <small>(Monday - Sunday)</small>",
-                            "category": "fashion",
-                            "link": "https://1.envato.market/R5Nv",
-                            "x": "0.4785",
-                            "y": "0.2873"
+                            "description": "<p>データベースより、本日の作業量及び現在の作業人数を動的に取得し表示しています。</p>",
+                            "category": "food",
+                            "style": "dark",
+                            "x": "0.2390",
+                            "y": "0.3000",
+                            
+                            
+                        },
+                        {
+                            "id": "area5",
+                            "title": "<h4>Area5</h4>",
+                            "about": "Lorem ipsum",
+                            "description": "<p>まだ、データは取得できません。</p><p>改良中</p>",
+                            "category": "food",
+                            "style": "dark",
+                            "x": "0.6374",
+                            "y": "0.3000",
+                        },
+                        {
+                            "id": "area6",
+                            "title": "<h4>Area6</h4>",
+                            "about": "Lorem ipsum",
+                            "description": "<p>まだ、データは取得できません。</p><p>改良中</p>",
+                            "category": "food",
+                            "style": "dark",
+                            "x": "0.4500",
+                            "y": "0.3000",
                         }
+                        
                     ]
                 },
                 {
@@ -256,95 +163,46 @@ class SiteController < ApplicationController
                     "locations": [
                         {
                             "id": "area4",
-                            "title": "area4's",
+                            "title": "<h4>Area4</h4>",
                             "about": "Lorem ipsum",
-                            "description": "<p>Recognizing the need is the primary condition for design.</p><strong>10:00 - 22:00</strong> <small>(Monday - Sunday)</small>",
-                            "image": "https://source.unsplash.com/fDzQTTDI7bs/400x150",
+                            "description": "<p>まだ、データは取得できません。</p><p>改良中</p>",
                             "category": "food",
                             "style": "dark",
-                            "x": "0.7536",
-                            "y": "0.2196"
+                            "x": "0.2390",
+                            "y": "0.6000"
                         },
                         {
                             "id": "area3",
-                            "title": "<p>現在の作業人数：#{n_of_staffs}</p><p>本日の作業量：#{n_of_jobs}",
+                            "title": "<h4>Are3</h4>",
                             "about": "Lorem ipsum",
-                            "description": "<p>現在の作業人数：#{n_of_staffs}</p><p>本日の作業量：#{n_of_jobs}",
+                            "description": "<p>まだ、データは取得できません。</p><p>改良中</p>",
                             "category": "food",
                             "style": "dark",
                             "x": "0.2390",
                             "y": "0.3000",
-                            "link": "https://1.envato.market/R5Nv"
+                            
                         },
                         {
-                            "id": "mcdonalds",
-                            "title": "McDonald's",
+                            "id": "area1",
+                            "title": "<h4>Area1</h4>",
                             "about": "Lorem ipsum",
-                            "description": "Additional information",
+                            "description": "<p>まだ、データは取得できません。</p><p>改良中</p>",
                             "category": "food",
-                            "link": "https://1.envato.market/R5Nv",
-                            "x": "0.7374",
-                            "y": "0.3918"
-                        },
-                        {
-                            "id": "pizzahut",
-                            "title": "Pizza Hut",
-                            "about": "Lorem ipsum",
-                            "description": "Make it great",
-                            "category": "food",
-                            "x": "0.7092",
-                            "y": "0.5232"
-                        },
-                        {
-                            "id": "subway",
-                            "title": "Subway",
-                            "about": "Lorem ipsum",
-                            "description": "Eat fresh.",
-                            "category": "food",
-                            "x": "0.6278",
-                            "y": "0.2731"
-                        },
-                        {
-                            "id": "cvs",
-                            "title": "CVS Pharmacy",
-                            "about": "Lorem ipsum",
-                            "description": "<p>Recognizing the need is the primary condition for design.</p><strong>7:30 - 22:00</strong> <small>(Monday - Sunday)</small>",
-                            "category": "health",
-                            "link": "https://1.envato.market/R5Nv",
-                            "thumbnail": "CVS",
-                            "x": "0.5104",
-                            "y": "0.2471"
-                        },
-                        {
-                            "id": "pullnbear",
-                            "title": "Pull & Bear",
-                            "about": "Lorem ipsum",
-                            "description": "<p>Recognizing the need is the primary condition for design.</p><strong>7:30 - 22:00</strong> <small>(Monday - Sunday)</small>",
-                            "category": "fashion",
-                            "thumbnail": "PB",
-                            "x": "0.4846",
-                            "y": "0.2946"
-                        },
-                        {
-                            "id": "amc",
-                            "title": "AMC Theatres",
-                            "about": "Lorem ipsum",
-                            "description": "Additional information",
-                            "category": "others",
                             "style": "dark",
-                            "thumbnail": "AMC",
-                            "x": "0.6671",
-                            "y": "0.6851"
+                            "x": "0.6374",
+                            "y": "0.3000",
                         },
                         {
-                            "id": "atnt",
-                            "title": "AT&T",
+                            "id": "area2",
+                            "title": "<h4>Area2</h4>",
                             "about": "Lorem ipsum",
-                            "description": "Additional information",
-                            "category": "others",
-                            "x": "0.3750",
-                            "y": "0.5391"
+                            "description": "<p>まだ、データは取得できません。</p><p>改良中</p>",
+                            "category": "food",
+                            "style": "dark",
+                            "x": "0.4500",
+                            "y": "0.3000",
                         }
+                        
                     ]
                 }
             ]
